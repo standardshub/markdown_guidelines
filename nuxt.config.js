1da84d77f5be144e289ac30d88d524e98cbe655c
Joaquin Prado <jprado@standardshub.io>
2022-06-06 12:20:38 -0700

    update nuxt.config.js file

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,7 +1,12 @@
 import theme from '@nuxt/content-theme-docs'
 
 export default theme({
+  ssr: false,
+  target: 'static',
   docs: {
     primaryColor: '#E24F55'
+  },
+  router: { 
+    base: '/md2html_guidelines/' 
   }
 })
